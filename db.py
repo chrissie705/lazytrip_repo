b778eaca353988d6eb93d2bcc1a08866efde10fc
Aurélie Largent <aurelie.largent@abbeal.com>
2019-02-23 18:50:08 +0100

    base project

---
+++ db.py
@@ -0,0 +1,13 @@
+import sys
+import pymongo
+from pymongo import MongoClient
+
+connection = MongoClient("ds149344.mlab.com", 49344)
+db = connection["lazytrip_dev"]
+db.authenticate("test", "firstival1")
+
+activities = db.activities
+
+for activity in activities.find({}, {"name": 1}):
+  print(activity)
+
